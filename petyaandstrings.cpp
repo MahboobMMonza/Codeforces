b98f963c00528567b51f1f7a139b043a21990884
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2022-02-21 17:26:02 -0500

    Petya and Strings

---
+++ petyaandstrings.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int (i) = (a); (i) < (b); (i) += (c))
+#define forae(i, a, b, c) for (int (i) = (a); (i) <= (b); (i) += (c))
+#define ford(i, a, b, c) for (int (i) = (a); (i) > (b); (i) -= (c))
+#define forde(i, a, b, c) for (int (i) = (a); (i) >= (b); (i) -= (c))
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    fio;
+    string s1, s2;
+    cin >> s1 >> s2;
+    fora(i, 0, s1.length(), 1) {
+        if (s1[i] > 'Z') {
+            s1[i] -= 32;
+        }
+        if (s2[i] > 'Z') {
+            s2[i] -= 32;
+        }
+        if (s1[i] < s2[i]) {
+            cout << -1;
+            return 0;
+        } else if (s1[i] > s2[i]) {
+            cout << 1;
+            return 0;
+        }
+    }
+    cout << 0;
+    return 0;
+}
