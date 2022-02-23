2c66c9bf9f735eeaa30a24c8fbb3601b6cb6ee97
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2022-02-23 07:55:37 -0500

    Great sequence

---
+++ greatsequence.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int (i) = (a); (i) < (b); (i) += (c))
+#define forae(i, a, b, c) for (int (i) = (a); (i) <= (b); (i) += (c))
+#define ford(i, a, b, c) for (int (i) = (a); (i) > (b); (i) -= (c))
+#define forde(i, a, b, c) for (int (i) = (a); (i) >= (b); (i) -= (c))
+#define dblinf numeric_limits<double>::infinity()
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<double, double> pd;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    fio;
+    int t, n, x, red;
+    ll ans = 0;
+    vector<ll> nums(2e5 + 2);
+    map<ll, int> freq;
+    cin >> t;
+    while (t--) {
+        ans = 0;
+        cin >> n >> x;
+        freq.clear();
+        fora(i, 0, n, 1) {
+            cin >> nums[i];
+            freq[nums[i]]++;
+        }
+        for (auto &a : freq) {
+            if (a.sc != 0) {
+                if (freq.count(a.fs * x)) {
+                    red = min(a.sc, freq[a.fs * x]);
+                    a.sc -= red;
+                    freq[a.fs * x] -= red;
+                }
+                if (freq.count(a.fs / x)) {
+                    red = min(a.sc, freq[a.fs / x]);
+                    a.sc -= red;
+                    freq[a.fs / x] -= red;
+                }
+                ans += a.sc;
+                a.sc = 0;
+            }
+        }
+        cout << ans << edl;
+    }
+    return 0;
+}
