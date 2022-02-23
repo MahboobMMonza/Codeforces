e90cabc97cefb4616765900b87237aa1e6bba718
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2022-02-22 23:30:38 -0500

    IQ Test

---
+++ iqtest.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int (i) = (a); (i) < (b); (i) += (c))
+#define forae(i, a, b, c) for (int (i) = (a); (i) <= (b); (i) += (c))
+#define ford(i, a, b, c) for (int (i) = (a); (i) > (b); (i) -= (c))
+#define forde(i, a, b, c) for (int (i) = (a); (i) >= (b); (i) -= (c))
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    fio;
+    int n, pat, e, o = e = 0;
+    vector<int> nums(100);
+    cin >> n;
+    fora(i, 0, n, 1) {
+        cin >> nums[i];
+        nums[i] %= 2;
+        e += (nums[i] + 1) % 2;
+        o += nums[i];
+    }
+    pat = e > o;
+    fora(i, 0, n, 1) {
+        if (nums[i] == pat) {
+            cout << i + 1 << edl;
+            break;
+        }
+    }
+    return 0;
+}
