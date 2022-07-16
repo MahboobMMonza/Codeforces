70c784047e7ade71858ab4ae592cb356af0e000c
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2022-07-16 18:31:46 -0400

    VolBIT Formulas Blitz A rectangle

--- arectangle.cpp
+++ arectangle.cpp
@@ -74,15 +74,29 @@ inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y %
 
 int main() {
     fio;
-    int row = 0, col = 0, v;
-    forae(i, 1, 5, 1) {
-        forae(j, 1, 5 ,1) {
-            cin >> v;
-            if (v) {
-                cout << abs(3 - i) + abs(3 - j);
-                return 0;
-            }
-        }
-    }
+    vector<ll> x(2, 0), y(2, 0);
+    cin >> x[0] >> y[0] >> x[1] >> y[1];
+    // Shift the rectangle to have lower left hexagon centre at (0, 0)
+    x[1] -= x[0];
+    x[0] = 0;
+    y[1] -= y[0];
+    y[0] = 0;
+    /**
+     * The number of centres a column can have depends on whether its index is even or odd. For simplicity, using
+     * 0-index, the pattern breaks down as follows:
+     *
+     * Let n be the number of integers from 0 to y2. The shifting we did means that the height is actually just y2 now.
+     * Notice that for any given rectangle, the first column (column 0) has all its centres as even y-values.For the
+     * next column, all the centres are located on odd y-values. This process repeats until the end of the rectangle,
+     * which after shifting, will always end on an even x-value. So the total number of hexagons encapsulated is the sum
+     * of the product of the number of even-index columns multiplied by the number of even numbers in n and the product
+     * of the number of odd-index columns multiplied by the number of odd numbers in n. Since n includes 0, this needs
+     * to be accounted for. The number of even numbers from 1 to n is n/2, and to include 0, which is even, just add 1
+     * to this value. The number of odd numbers in the same range is just the difference of n and the number of even
+     * numbers. Remember the range is inclusive of 0, so n = y2 + 1.
+     *
+     */
+    ll h_even = y[1] / 2 + 1, even_col = x[1] / 2 + 1;
+    cout << (h_even * even_col) + ((y[1] + 1 - h_even) * (x[1] + 1 - even_col)) << edl;
     return 0;
 }
