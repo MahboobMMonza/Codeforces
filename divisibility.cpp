7735e75bdb9e5c06bd700421773c7bea84a71db3
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2022-07-18 00:30:22 -0400

    VolBIT Formulas Blitz Divisibility

---
+++ divisibility.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define forae(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define forde(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    fio;
+    /**
+     * Being divisible by all numbers from 2 to 10 inclusive means you only need to be divisible by 8 * 5 * 7 * 9.
+     * This is the LCM of all integers from 2 to 10 inclusive. Since integer division already floors, dividing
+     * n by this value will automatically output the result.
+     *
+     */
+    ull n;
+    cin >> n;
+    cout << n / (8ULL * 5ULL * 7ULL * 9ULL);
+    return 0;
+}
