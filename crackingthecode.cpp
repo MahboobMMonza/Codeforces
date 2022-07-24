cc9c343f6eaca0af5a69b690e8ee72741a347014
Mohammad Mahdi Mahboob <85646892+habibiLaFerrari97@users.noreply.github.com>
2022-07-24 02:35:40 -0400

    VolBIT Formulas Blitz Cracking the Code

---
+++ crackingthecode.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define forae(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define forde(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    fio;
+    /**
+     * Modular mathematics states that (a * b) mod m = ((a mod m) * (b mod m)) mod m. The logPow function above should
+     * give the desired value since exponentiation is repeated multiplication. Just ensure the numbers are shuffled
+     * correctly. Read the value as a string and then formulate the number afterwards. The value of m in this case is
+     * 10^5, since it gives any value from 0 to 99999, which would be a 5 digit number. For this problem, follow the
+     * instructions given, just employ the logPow function to get faster exponentiation.
+     *
+     */
+     string inp;
+     cin >> inp;
+     ll n = (ll) (inp[0] - '0') * 10000 + (ll) (inp[2] - '0') * 1000 + (ll) (inp[4] - '0') * 100 +
+             (ll) (inp[3] - '0') * 10 + (ll) (inp[1] - '0');
+     cout << setfill('0') << setw(5);
+     cout << logPow(n, 5LL, 100000LL) << edl;
+    return 0;
+}
